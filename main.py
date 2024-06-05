c9701b77c11abe61e8a80dc27275bebf32e998cf
Ana <mihajloviccana37@gmail.com>
2024-06-05 17:15:13 +0200

    added options for game and plot configuration inputs

--- main.py
+++ main.py
@@ -2,12 +2,24 @@
 import game
 import time_series_data
 import numpy as np
+import re
+import os
 from collections import Counter
 
-num_of_iters = 30 #int(input("Number of game iterations : "))
+num_of_iters = 30 
+file_name = input("Path to game configuration : ")
+c = input('Tactic for filtration plot : ')
+path = os.path.join('examples/', file_name)
+
+if re.search('examples/Simulated[1-9].txt', path):
+    game_instance = game.SimulatedGame(path)
+else :
+    game_instance = game.Game(path) 
+    print("game")
+
 
 time_series_data_instance = time_series_data.time_series_data()
-for time, state in game.Game("examples/Case2.txt"):
+for time, state in game_instance:
     if (time > num_of_iters):
         break
 
@@ -24,5 +36,5 @@ VR_complexes = time_series_data_instance.seriesToComplexes(3, 3)
 #VR_complexes['T'].printFiltration(300)
 #print("--------------------")
 
-VR_complexes['D'].plotPersistenceDiagram()
+VR_complexes[c].plotPersistenceDiagram()
 
